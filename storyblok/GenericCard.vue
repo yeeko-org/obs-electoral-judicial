d97ae03ec12669f9c5dadc314ead9ab3c863d4b2
Ricardo Sanginés <rickrebel@gmail.com>
2024-10-31 17:34:06 -0600

    real init commit

--- storyblok/GenericCard.vue
+++ storyblok/GenericCard.vue
@@ -1,55 +1,75 @@
-<script setup>
-import { computed, ref, onMounted } from 'vue'
+<script>
 import componentMixin from "~/mixins/componentMixin"
 import { useDisplay } from 'vuetify'
-import { StoryblokComponent } from 'storyblok-vue'
-
-// Setup responsive display properties
 const { xs, mdAndUp, smAndDown } = useDisplay()
 
-// Define props
-const props = defineProps({
-  blok: Object
-})
+export default {
+  name: 'GenericCard',
+  props: ['blok'],
+  data(){
+    return {
+      is_mounted: false,
+      is_left: false,
+    }
+  },
+  mixins: [componentMixin],
+  computed:{
+    xs: ()=> xs,
+    smAndDown: ()=> smAndDown,
+    smAndUp: ()=> mdAndUp,
+    style_main_div(){
+      return `margin-${this.blok.image_position}: ${this.blok.image_margin || 410}px`
+    },
+    position_top(){
+      return this.blok.image_position === 'top'
+    },
+    position_image(){
+      return this.is_image_top ?
+        this.blok.image_position === 'top'
+          ? 'center'
+          : 'top'
+        : this.blok.image_position
+    },
+    is_image_top(){
+      return smAndDown || this.blok.image_position === 'top'
+    },
+    max_heigth(){
+      return this.is_image_top
+        ? 180
+        : 400
+    },
+    image_iframe(){
+      return this.blok.video_side && this.blok.video_side.includes('youtube')
+    },
+    order_iframe(){
+      console.log("video_position", this.blok.video_position)
+      const video_position = this.blok.video_position || 'left'
+      return video_position === 'left' ? 1 : 3
+    },
+  },
+  mounted(){
+    this.is_mounted = true
+  },
+}
 
-// Initialize reactive data
-const is_mounted = ref(false)
-
-// Computed properties
-const style_main_div = computed(() => `margin-${props.blok.image_position}: ${props.blok.image_margin || 410}px`)
-const position_top = computed(() => props.blok.image_position === 'top')
-const position_image = computed(() => is_image_top.value ? (props.blok.image_position === 'top' ? 'center' : 'top') : props.blok.image_position)
-const is_image_top = computed(() => smAndDown.value || props.blok.image_position === 'top')
-const max_heigth = computed(() => is_image_top.value ? 180 : 400)
-const image_iframe = computed(() => props.blok.video_side && props.blok.video_side.includes('youtube'))
-const order_iframe = computed(() => {
-  console.log("video_position", props.blok.video_position)
-  const video_position = props.blok.video_position || 'left'
-  return video_position === 'left' ? 1 : 3
-})
-
-// Lifecycle hook
-onMounted(() => {
-  is_mounted.value = true
-})
 </script>
 
 <template>
   <v-col
-    v-editable="props.blok"
-    :cols="props.blok.cols"
-    :md="props.blok.md"
+    v-editable="blok"
+    :cols="blok.cols"
+    :md="blok.md"
     :class="xs ? 'pa-2' : 'pa-3'"
   >
     <v-card
-      :color="props.blok.background_color && props.blok.background_color.color || 'transparent'"
+      :color="blok.background_color && blok.background_color.color || 'transparent'"
       elevation="4"
       class="landing-max-width-section text-center rounded-xl"
     >
       <v-row v-if="image_iframe" no-gutters>
         <v-col cols="12" md="8" class="pa-0" :order="order_iframe" _order="3">
           <iframe
-            :src="props.blok.video_side"
+            :src="blok.video_side"
             title="YouTube video player"
             width="100%"
             :height="xs ? 200 : 400"
@@ -65,20 +85,27 @@ onMounted(() => {
           class="d-flex flex-column justify-center"
         >
           <v-card-title class="no-wrap">
-            {{props.blok.title}}
+            {{blok.title}}
           </v-card-title>
-          <v-card-text v-if="props.blok.description">
+          <v-card-text v-if="blok.description">
             <div class="d-flex flex-column">
               <div class="align-self-start mb-auto black--text text-left">
+<!--                <p-->
+<!--                  :class="{-->
+<!--                    'body-1': xs,-->
+<!--                    'mt-2': xs,-->
+<!--                    'mt-4': smAndUp,-->
+<!--                    'body-2': smAndUp}"-->
+<!--                >-->
                 <p>
-                  {{props.blok.description}}
+                  {{blok.description}}
                 </p>
               </div>
             </div>
           </v-card-text>
           <v-card-actions>
             <StoryblokComponent
-              v-for="blok in props.blok.button"
+              v-for="blok in blok.button"
               :key="blok._uid"
               :blok="blok"
             ></StoryblokComponent>
@@ -89,11 +116,11 @@ onMounted(() => {
         <v-img
           :style="`height: ${max_heigth}px`"
           dark
-          :src="resizedImg(props.blok.image_side)"
+          :src="resizedImg(blok.image_side)"
           _min-height="400"
           id="header"
           :contain="!is_image_top"
-          _position="is_image_top ? 'center' : props.blok.image_position"
+          _position="is_image_top ? 'center' : blok.image_position"
           :position="position_image"
           :class="{
             'py-4': xs,
@@ -105,9 +132,9 @@ onMounted(() => {
             class="black--text d-flex flex-column justify-center py-6 px-2"
           >
             <v-card-title class="no-wrap">
-              {{props.blok.title}}
+              {{blok.title}}
             </v-card-title>
-            <v-card-text v-if="props.blok.description">
+            <v-card-text v-if="blok.description">
               <div class="d-flex flex-column">
                 <div class="align-self-start mb-auto black--text text-left">
                   <p
@@ -117,14 +144,14 @@ onMounted(() => {
                     'mt-4': smAndUp,
                     'body-2': smAndUp}"
                   >
-                    {{props.blok.description}}
+                    {{blok.description}}
                   </p>
                 </div>
               </div>
             </v-card-text>
             <v-card-actions>
               <StoryblokComponent
-                v-for="blok in props.blok.button"
+                v-for="blok in blok.button"
                 :key="blok._uid"
                 :blok="blok"
               ></StoryblokComponent>
@@ -133,9 +160,9 @@ onMounted(() => {
         </v-img>
         <div v-if="is_image_top" class="py-6 px-2">
         <v-card-title class="no-wrap">
-          {{props.blok.title}}
+          {{blok.title}}
         </v-card-title>
-        <v-card-text v-if="props.blok.description">
+        <v-card-text v-if="blok.description">
           <div class="d-flex flex-column">
             <div class="align-self-start mb-auto black--text text-left">
               <p
@@ -145,14 +172,14 @@ onMounted(() => {
                 'mt-4': smAndUp,
                 'body-2': smAndUp}"
               >
-                {{props.blok.description}}
+                {{blok.description}}
               </p>
             </div>
           </div>
         </v-card-text>
         <v-card-actions>
           <StoryblokComponent
-            v-for="blok in props.blok.button"
+            v-for="blok in blok.button"
             :key="blok._uid"
             :blok="blok"
           ></StoryblokComponent>
@@ -163,5 +190,5 @@ onMounted(() => {
   </v-col>
 </template>
 
-<style scoped lang="scss">
+<style lang="scss" scoped>
 </style>
