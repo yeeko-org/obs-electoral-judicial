c7f619ddebad758d51ef21a435817c494bf9aa95
Ricardo Sanginés <rickrebel@gmail.com>
2025-06-20 13:59:26 -0600

    dejo listo para clonación artificial

---
+++ layouts/banner.vue
@@ -0,0 +1,28 @@
+<script setup>
+
+import {nextTick, onMounted} from "vue";
+const storyblokApi = useStoryblokApi();
+
+const { $preview } = useNuxtApp()
+
+
+const version = $preview ? 'draft' : 'published';
+
+</script>
+
+<template>
+  <v-app>
+    <v-responsive max-width="1024" class="mx-auto">
+      <v-main class="" id="app-width">
+        <v-container class="_px-1 _px-sm-3 px-0 pt-0" fluid>
+          <NuxtPage />
+        </v-container>
+      </v-main>
+    </v-responsive>
+<!--    <Footer />-->
+  </v-app>
+</template>
+
+<style lang="scss">
+@use '../assets/css/utils.scss' as *;
+</style>
---
+++ pages/[page_name].vue
@@ -0,0 +1,34 @@
+<script setup>
+const { $preview } = useNuxtApp()
+const { page_name } = useRoute().params
+definePageMeta({
+  // middleware: 'dashboard',
+  layout: 'banner',
+})
+
+// const version_sb = process.env.NUXT_PUBLIC_STORYBLOK_VERSION
+const version = $preview ? 'draft' : 'published'
+console.log('page_name', page_name)
+const story = await useAsyncStoryblok(
+    `${page_name}`,
+    { version: version },
+{
+  customParent: 'https://app.storyblok.com',
+  cache: 'no-cache',
+}
+)
+
+console.log('story', story)
+
+// useSeoMeta({
+//
+// })
+
+
+</script>
+
+<template>
+  <StoryblokComponent
+    v-if="story" :blok="story.content"
+  />
+</template>
